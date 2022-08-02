65f0c43996cbf8984fb3de61545d70b857d5a9a7
NamiStuff <36270268+NamiStuff@users.noreply.github.com>
2022-08-02 14:54:42 +0200

    Update app.js

--- test/js/app.js
+++ test/js/app.js
@@ -68,7 +68,7 @@ const Page = {
 m.mount(document.getElementById('page'), Page);
 
 var iconList = document.getElementsByClassName('icon-code');
-var icon = document.getElementsByClassName('icon-md')
+var icon = document.getElementsByClassName('icon-md');
 for (var i=0; i<iconList.length; i++) {
-	iconList[i].innerHTML = '\\' + getComputedStyle(icon[i], ':before').content.slice(1, -1).codePointAt(0).toString(16);   
+	iconList[i].innerHTML = '\\' + getComputedStyle(icon[0], ':before').content.slice(1, -1).codePointAt(0).toString(16);   
 }
