94cc1a653a99c942d64e8b6db311125664c25209
NamiStuff <36270268+NamiStuff@users.noreply.github.com>
2026-03-06 19:25:46 +0100

    Update app.js

--- test/js/app.js
+++ test/js/app.js
@@ -1,6 +1,6 @@
 //const fontClass = 'gi',
 //      iconClass = 'gi';
-import { fontClass, iconClass } from 'download-and-format-icons.js';
+import { fontClass, iconClass } from '/download-and-format-icons.js';
 
 const Data = {
     glyphs: () => m.request({ method: 'GET', url: 'data/glyphs.json' })
